1f6d5f626e2cdb8b2324a6db79bbdaa04f866c18
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-20 21:18:49 +0530

    [chiru] Checking BubbleSort

--- Algorithm_Problems/Program.cs
+++ Algorithm_Problems/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
 
-            int[] arr = { 5,3,2,6,9,1,4,8,7 };
+            int[] arr = { 5,3,2,6,9,1,4,8,7,10 };
             BubbleSort.BubbleSorting(arr);
 
             Console.WriteLine("Sorted array");

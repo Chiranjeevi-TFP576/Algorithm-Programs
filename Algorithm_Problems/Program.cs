cdb7f6f66d826f4815d59db8e028b80bf0214230
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-20 22:08:24 +0530

    [chiru] add BinarySearch

--- Algorithm_Problems/Program.cs
+++ Algorithm_Problems/Program.cs
@@ -4,13 +4,41 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome To Algorithm Problems");
+            bool end = true;
+            Console.WriteLine("1.BubbleSort\n2.Binary Search\n3.End Of Programme");
+            while (end)
+            {
+                Console.Write("Choose an option to execute : ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        int[] arr = { 5, 3, 2, 6, 9, 1, 4, 8, 7, 10 };
+                        BubbleSort.BubbleSorting(arr);
 
-            int[] arr = { 5,3,2,6,9,1,4,8,7,10 };
-            BubbleSort.BubbleSorting(arr);
+                        Console.WriteLine("Sorted array");
+                        BubbleSort.DisplayArray(arr);
+                        break;
+                    case 2:
+                        BinarySearch binarysearch = new BinarySearch();
+                        binarysearch.Search(@"D:\Batch 244\Algorithm-Programs\Algorithm_Problems\BinarySearch.txt");
+                        break;
+                    case 3:
+                        end = false;
+                        break;
 
-            Console.WriteLine("Sorted array");
-            BubbleSort.DisplayArray(arr);
+                    default:
+                        Console.WriteLine("You choose Invalid Option");
+                        break;
 
+
+                }
+            }
+
+            
+
+            
 
 
         }

ae54b74f8e6206424e0b79a80c239d263115dfe9
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-20 21:17:04 +0530

    [chiru] Check BubblrSort

---
+++ Algorithm_Problems/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithm_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 5,3,2,6,9,1,4,8,7 };
+            BubbleSort.BubbleSorting(arr);
+
+            Console.WriteLine("Sorted array");
+            BubbleSort.DisplayArray(arr);
+
+
+
+        }
+    }
+}
